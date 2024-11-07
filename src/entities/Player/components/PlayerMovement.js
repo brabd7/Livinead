aca1d406da37b40810526ee4957316115c6503a1
brabd7 <br.abd7@gmail.com>
2024-11-07 22:34:21 +0100

    Avancer, reculer, droite et gauche fait !

--- src/entities/Player/components/PlayerMovement.js
+++ src/entities/Player/components/PlayerMovement.js
@@ -5,20 +5,26 @@ export class PlayerMovement {
         this.player = player;
         this.THREE = player.THREE;
         this.cameraController = player.playerCameraController.cameraController;
+        this.playerBody = this.player.playerPhysics.body;
 
         // Paramètres
+        this.moveSpeed = 15;
+
         this.moveForward = false;
         this.moveBackward = false;
         this.moveRight = false;
         this.moveLeft = false;
 
         this.currentPlayerDirection = new this.THREE.Vector3();
+        this.rightOfCurrentPlayerDirection = new this.THREE.Vector3();
     }
 
     update()
     {
         this.getCurrentPlayerDirection();
-        this.moveForwardMovement();
+        this.getRightOfCurrentPlayerDirection();
+        this.forwardMovement();
+        this.rightMovement();
     }
 
     getCurrentPlayerDirection()
@@ -26,11 +32,41 @@ export class PlayerMovement {
         this.cameraController.getDirection(this.currentPlayerDirection);
     }
 
-    moveForwardMovement()
+    getRightOfCurrentPlayerDirection()
+    {
+        this.rightOfCurrentPlayerDirection.crossVectors(this.currentPlayerDirection, new this.THREE.Vector3(0, 1, 0)).normalize();
+    }
+
+    forwardMovement()
     {
         if (this.moveForward)
         {
-            console.log('avancer')
+            this.playerBody.velocity.x = this.currentPlayerDirection.x * this.moveSpeed;
+            this.playerBody.velocity.z = this.currentPlayerDirection.z * this.moveSpeed;
+        }
+        else if (this.moveBackward)
+        {
+            this.playerBody.velocity.x = -this.currentPlayerDirection.x * this.moveSpeed;
+            this.playerBody.velocity.z = -this.currentPlayerDirection.z * this.moveSpeed;
+        }
+        else 
+        {
+            this.playerBody.velocity.x = 0;
+            this.playerBody.velocity.z = 0;
+        }
+    }
+
+    rightMovement()
+    {
+        if (this.moveRight)
+        {
+            this.playerBody.velocity.x += this.rightOfCurrentPlayerDirection.x * this.moveSpeed;
+            this.playerBody.velocity.z += this.rightOfCurrentPlayerDirection.z * this.moveSpeed;
+        }
+        else if (this.moveLeft)
+        {
+            this.playerBody.velocity.x -= this.rightOfCurrentPlayerDirection.x * this.moveSpeed;
+            this.playerBody.velocity.z -= this.rightOfCurrentPlayerDirection.z * this.moveSpeed;
         }
     }
 }
